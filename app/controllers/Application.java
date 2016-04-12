e6d1fcb1eafa05b7874101c620c6980c7e00f0e8
Administrator <Administrator@-----mayue---->
2016-04-12 13:25:11 +0800

    Hello World 链接地址：http://localhost:9000/sayhello/

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -8,9 +8,12 @@ import java.util.*;
 import models.*;
 
 public class Application extends Controller {
-
-    public static void index() {
-        render();
+	//action方法，此方法响应请求，并返回请求页面
+	public static void sayhello() {
+        String message = "Hello World!";
+        // 设置属性
+        renderArgs.put("message", message);
+        //返回页面
+        render("/Application/index.html");
     }
-
-}
+} 
